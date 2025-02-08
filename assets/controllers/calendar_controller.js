4e5b2db133fd4d09248c53893097a2a8cbce9b5d
ludovicjj <jahanlud@gmail.com>
2025-02-08 21:16:19 +0100

    init calendar

---
+++ assets/controllers/calendar_controller.js
@@ -0,0 +1,221 @@
+import { Controller } from '@hotwired/stimulus';
+import '../styles/calendar.css';
+import {Events} from "../js/data.js";
+import {addDays,
+    diffInDay,
+    endOfMonth,
+    endOfWeek,
+    getDayId,
+    getDaysBetween,
+    minDates,
+    startOfWeek
+} from "../js/functions/date.js";
+
+/** @typedef {{name: string, start: Date, end: Date, fullDay?: boolean, type?: string}} CalendarEvent */
+
+const dayFormatter = new Intl.DateTimeFormat(undefined, {weekday: 'short'}) // long
+const timeFormatter = new Intl.DateTimeFormat(undefined, {hour: '2-digit', minute: '2-digit'})
+const monthFormatter = new Intl.DateTimeFormat(undefined, {month: 'long'})
+
+export default class extends Controller {
+
+    static targets = [ "root", "date" ]
+
+    /** @type {Map<string, CalendarEvent[]>} */
+    #eventsMap = new Map()
+
+    connect() {
+        this.#eventsMap = new Map()
+        this.month = new Date().getMonth()
+        this.year = new Date().getFullYear()
+
+        if (!this.hasRootTarget) {
+            console.error('missing root element')
+        }
+
+        this.#fillEventMap(Events)
+        this.#render()
+    }
+
+    prev() {
+        this.month -= 1
+        this.#navigate()
+    }
+    next() {
+        this.month += 1
+        this.#navigate()
+    }
+
+    /**
+     * @param {Date} curentDate
+     */
+    #showCurrentDate(curentDate) {
+        this.dateTarget.innerHTML = `${monthFormatter.format(curentDate)} ${curentDate.getFullYear()}`
+    }
+
+    #navigate() {
+        if (this.month < 0) {
+            this.month = 11;
+            this.year -= 1;
+        } else if (this.month > 11) {
+            this.month = 0;
+            this.year += 1;
+        }
+
+        this.#render();
+    }
+
+    #render() {
+        const curentDate = new Date(this.year, this.month, 1)
+        const firstDayOfMonth = new Date(this.year, this.month, 1, 0, 0, 0, 0);
+        const start = startOfWeek(firstDayOfMonth);
+        const end = endOfWeek(endOfMonth(firstDayOfMonth));
+        const daysOfMonth = getDaysBetween(start, end);
+
+        const days = Array.from(
+            {length: 7},
+            (_, i) => dayFormatter.format(addDays(start, i))
+        );
+
+        this.#showCurrentDate(curentDate)
+
+
+        this.rootTarget.innerHTML = `
+            <table class="calendar">
+            <thead>
+                <tr>
+                    ${days.map(day => `<th>${day}</th>`).join('')}
+                </tr>
+            </thead>
+                <tbody>
+                </tbody>
+            </table>`
+
+        /** @type {Map<CalendarEvent, position>} */
+        const positionMap = new Map()
+        const tbody = this.rootTarget.querySelector('tbody')
+        let tr = document.createElement('tr')
+
+        for (const dayOfMonth of daysOfMonth) {
+            const td = this.#buildCell(dayOfMonth, this.month, positionMap)
+            tr.append(td)
+            if (dayOfMonth.getDay() === 0) {
+                tbody.append(tr)
+                tr = document.createElement('tr')
+                positionMap.clear()
+            }
+        }
+    }
+
+    #buildCell(date, month, positionMap) {
+        const getAvailablePosition = () => {
+            if (positionMap.size === 0) {
+                return 0;
+            }
+            const positions = Array.from(positionMap.values());
+            const max = Math.max(...positions);
+            for (let i = 0; i < max; i++) {
+                if (!positions.includes(i)) {
+                    return i;
+                }
+            }
+            return max + 1;
+        }
+        const td = document.createElement('td')
+        const dayId = getDayId(date)
+        const currentDayId = getDayId(new Date())
+        const isCurrentMonth = date.getMonth() === month
+        const isCurrentDay = currentDayId === dayId
+
+        if (isCurrentDay) {
+            td.classList.add('current-day')
+        }
+
+        td.innerHTML = `<div class="calendar_cell">
+            <div class="calendar_date ${isCurrentMonth ? '' : 'calendar_date-diff'}">${date.getDate()}</div>
+            <div class="calendar_events"></div>
+        </div>`
+
+        const eventContainer = td.querySelector('.calendar_events')
+        const events = this.#eventsMap.get(dayId) || []
+        const finishedEvents = [];
+
+        for (const event of events) {
+            const classes = ['calendar_event']
+            if (event.type) {
+                classes.push('calendar_event-' + event.type)
+            }
+
+
+            // Début d'un évènement sur plusieurs jours
+            if (
+                event.fullDay &&
+                (dayId === getDayId(event.start) || date.getDay() === 1)
+            ) {
+                const position = getAvailablePosition()
+                positionMap.set(event, position);
+                const endDate =  minDates([event.end, endOfWeek(date)])
+
+                const days = diffInDay(date, endDate)
+
+                if (dayId !== getDayId(event.start)) {
+                    classes.push('calendar_event-overflow-left')
+                }
+                if (endDate !== event.end) {
+                    classes.push('calendar_event-overflow-right')
+                }
+                classes.push('calendar_event-fullday')
+
+                eventContainer.insertAdjacentHTML(
+                    'beforeend',
+                    `<div 
+                        class="${classes.join(' ')}"
+                        style="--days:${days}; --offset:${position}"
+                    >
+                        ${event.name}
+                    </div>`
+                )
+            }
+
+            if (event.fullDay && dayId === getDayId(event.end)) {
+                finishedEvents.push(event)
+            }
+
+            if (!event.fullDay) {
+                classes.push('calendar_event-hour')
+                eventContainer.insertAdjacentHTML('beforeend', `<div class="${classes.join(' ')}">
+                    <span>${timeFormatter.format(event.start)} - ${event.name}</span>
+                </div>`)
+            }
+        }
+
+        eventContainer.style.setProperty('--offset', (Math.max(...positionMap.values()) + 1).toString())
+
+        for (const event of finishedEvents) {
+            positionMap.delete(event)
+        }
+
+        return td
+    }
+
+    /**
+     * @param {CalendarEvent[]} events
+     */
+    #fillEventMap(events) {
+        const sortEvents = [...events].sort((a, b) => a.start < b.start ? -1 : 1)
+
+        for (const event of sortEvents) {
+            const days = getDaysBetween(event.start, event.end)
+            for (const day of days) {
+                const id = getDayId(day)
+                this.#eventsMap.set(
+                    id,
+                    [
+                        ...(this.#eventsMap.get(id) || []),
+                        event
+                    ]
+                )
+            }
+        }
+    }
+}
---
+++ assets/js/functions/date.js
@@ -0,0 +1,171 @@
+const weekStartOn = 1
+const msInDay = 86400000
+
+/**
+ * Renvoie la date du début de semaine
+ * @param {Date} date
+ * @return {Date}
+ */
+export function startOfWeek(date) {
+    const d = new Date(date)
+
+    // 0	Dimanche
+    // 1	Lundi
+    // 2	Mardi
+    // 3	Mercredi
+    // 4	Jeudi
+    // 5	Vendredi
+    // 6	Samedi
+    // Le premier jour du mois de février est un samedi (6)
+    const day = d.getDay()
+    // Il manque donc 5 jours pour arriver au premier jour de la semaine, c'est-à dire lundi (1)
+
+    // si le premier jour du mois est un samedi
+    // 0 + 6 - 1
+    // 5 (il manque 5 jours pour aller à lundi)
+
+    // si le premier jour du mois était un dimanche
+    // 7 + 0 - 1
+    // 6 (il manque 6 jours pour aller à lundi)
+
+    // Get diff between the given date and the start of the week
+    const diff = (day < weekStartOn ? 7 : 0) + day - weekStartOn
+    d.setDate(d.getDate() - diff)
+    d.setHours(0, 0, 0, 0)
+
+    return d
+}
+
+/**
+ * Renvoie la date en fin de semaine
+ * @param {Date} date
+ * @return {Date}
+ */
+export function endOfWeek(date) {
+    const d = new Date(date)
+
+    // 0	Dimanche
+    // 1	Lundi
+    // 2	Mardi
+    // 3	Mercredi
+    // 4	Jeudi
+    // 5	Vendredi
+    // 6	Samedi
+    // Le premier jour du mois de février est un samedi (6)
+    const day = d.getDay()
+    // Il manque donc 1 jour pour arriver au dernier jour de la semaine (0 - dimanche)
+
+    // Get diff between the given date and the end of the week
+    // 0 + 6 - (6 - 1)
+    // 0 + 6 - 5
+    // 1 (il faut ajouter 5 jours pour aller jusqu'à dimanche)
+
+    // si le premier jour du mois était un dimanche
+    // -7 + 6 - (0 - 1)
+    // -1 - (-1)
+    // -1 + 1
+    // 0 (il faut ajouter 0 jour pour aller jusqu'à dimanche)
+
+    const diff = (day < weekStartOn ? -7 : 0) + 6 - (day - weekStartOn)
+    d.setDate(d.getDate() + diff)
+    d.setHours(23, 59, 59, 999)
+
+    return d
+}
+
+/**
+ * Renvoie la date de fin de mois
+ * @param {Date} date
+ * @return {Date}
+ */
+export function endOfMonth(date) {
+    const d = new Date(date)
+
+    // ajoute un mois
+    d.setMonth(d.getMonth() + 1)
+
+    // positionne la date au dernier jour du mois précédent.
+    d.setDate(0)
+    d.setHours(23, 59, 59, 999)
+
+    return d
+}
+
+/**
+ * Ajoute n jour(s) à une date
+ * @param {Date} date
+ * @param {number} n
+ * @return {Date}
+ */
+export function addDays(date, n) {
+    const d = new Date(date)
+    d.setDate(d.getDate() + n)
+
+    return d
+}
+
+/**
+ * Renvoie les jours entre les deux dates
+ * @param {Date} start
+ * @param {Date} end
+ * @return {Date[]}
+ */
+export function getDaysBetween(start, end) {
+    if (start > end) {
+        throw new Error('La date de debut ne peux pas être supérieur a la date de fin')
+    }
+
+    const days = [];
+    let cursor = new Date(start)
+    cursor.setHours(0, 0 , 0, 0)
+
+    while (cursor < end) {
+        days.push(cursor)
+        cursor = addDays(cursor, 1)
+    }
+
+    return days
+}
+
+/**
+ * Renvoie un identifiant pour un jour
+ * @param {Date} date
+ * @return {string}
+ */
+export function getDayId(date) {
+    return `${date.getFullYear()}-${date.getMonth().toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`
+}
+
+/**
+ * Trouve le nombre de jours en calendrier entre 2 dates
+ * @param {Date} start
+ * @param {Date} end
+ * @return {number}
+ */
+export function diffInDay(start, end) {
+    const a = new Date(start)
+    a.setHours(0, 0, 0, 0)
+    const b = new Date(end)
+    b.setHours(23, 59, 59, 999)
+
+    return Math.round((b.getTime() - a.getTime()) / msInDay)
+}
+
+/**
+ * Trouve la date la plus loin dans le temps
+ * @param {Date[]} dates
+ */
+export function minDates(dates) {
+    if (dates.length === 0) {
+        throw new Error("Impossible de trouver le minimum sans dates");
+    }
+
+    let min = dates[0];
+    for (const date of dates) {
+        if (date < min) {
+            min = date;
+        }
+    }
+
+    return min;
+}
