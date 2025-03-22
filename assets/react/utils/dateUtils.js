cebd1667149920e2116274a5ebea4eaa9d2cbba9
ludovicjj <jahanlud@gmail.com>
2025-03-22 15:42:55 +0100

    fix getDayId

--- assets/react/utils/dateUtils.js
+++ assets/react/utils/dateUtils.js
@@ -168,7 +168,7 @@ export function endOfWeek(date) {
  * @return {string}
  */
 export function getDayId(date) {
-    return `${date.getFullYear()}-${date.getMonth().toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`
+    return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')}`
 }
 
 /**
