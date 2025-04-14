89526228d810ddec11d7c06111865581870c6f99
ludovicjj <jahanlud@gmail.com>
2025-04-14 22:21:29 +0200

    #8 Navbar

--- assets/controllers/menu_controller.js
+++ assets/controllers/menu_controller.js
@@ -2,24 +2,15 @@ import { Controller } from '@hotwired/stimulus';
 import '../styles/menu.css'
 
 export default class extends Controller {
-    connect() {
-        this.element.addEventListener('click', async (e) => {
-            if (
-                e.target.closest('a') ||
-                e.target.closest('img')
-            ) {
-                return;
-            }
+    toggleMenu(e) {
+        const isOpen = this.element.classList.contains('open')
 
-            const isOpen = this.element.classList.contains('open')
-
-            if (isOpen) {
-                this.element.classList.remove('open')
-                this.element.classList.add('close')
-            } else {
-                this.element.classList.add('open')
-                this.element.classList.remove('close')
-            }
-        })
+        if (isOpen) {
+            this.element.classList.remove('open')
+            this.element.classList.add('close')
+        } else {
+            this.element.classList.add('open')
+            this.element.classList.remove('close')
+        }
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -58,6 +58,10 @@ Encore
         config.corejs = '3.38';
     })
     .enablePostCssLoader()
+    .copyFiles({
+        from: './assets/img',
+        to: 'images/[path][name].[hash:8].[ext]'
+    })
 
     // enables Sass/SCSS support
     //.enableSassLoader()
